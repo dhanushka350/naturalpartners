7ec62641810a4913a6d63fc84d03f82bc5ce66db
dhanushka350 <dhanushka@akvasoft.com>
2019-01-07 16:40:08 +0530

    complete code

--- src/main/java/com/akvasoft/natural_partners/NaturalPartnersApplication.java
+++ src/main/java/com/akvasoft/natural_partners/NaturalPartnersApplication.java
@@ -2,9 +2,10 @@ package com.akvasoft.natural_partners;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class NaturalPartnersApplication {
+public class NaturalPartnersApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(NaturalPartnersApplication.class, args);
--- src/main/java/com/akvasoft/natural_partners/config/Scrape.java
+++ src/main/java/com/akvasoft/natural_partners/config/Scrape.java
@@ -19,6 +19,14 @@ import java.util.List;
 public class Scrape implements InitializingBean {
     private FirefoxDriver driver = null;
     private FirefoxDriver driver2 = null;
+    private boolean animal_health = true;
+    private boolean prescription_roducts = true;
+    private boolean acupuncture_oriental_medicine = true;
+    private boolean personal_care = false;
+    private boolean herbs = false;
+    private boolean vitamins_and_minerals = false;
+    private boolean nutritional_supplements = false;
+    private boolean homeopathy = false;
 
     @Autowired
     private ProductRepo productRepo;
@@ -27,7 +35,7 @@ public class Scrape implements InitializingBean {
     public void afterPropertiesSet() throws InterruptedException {
         System.setProperty("webdriver.gecko.driver", "/var/lib/tomcat8/geckodriver");
         FirefoxOptions options = new FirefoxOptions();
-        options.setHeadless(false);
+        options.setHeadless(true);
         driver = new FirefoxDriver(options);
         driver2 = new FirefoxDriver(options);
         JavascriptExecutor executor = (JavascriptExecutor) driver;
@@ -51,84 +59,128 @@ public class Scrape implements InitializingBean {
         list.add(nutritional_supplements);
         list.add(homeopathy);
 
-        int count = 1;
-        for (String s : list) {
-            String cate = getCurrentCategory(count);
-            driver.get(s);
-            Thread.sleep(5000);
-            WebElement main = driver.findElementByXPath("/html/body/div[4]/div/div/div/div[2]").findElement(By.tagName("section")).findElements(By.xpath("./*")).get(1);
-            WebElement select = main.findElement(By.tagName("form")).findElement(By.tagName("div")).findElement(By.className("results")).findElement(By.className("sellers")).findElement(By.tagName("select"));
-            new Select(select).selectByVisibleText("64");
-
-            boolean firstAttempt = false;
-            boolean endCategory = false;
-            while (true) {
-
-                while (!firstAttempt) {
-                    Thread.sleep(5000);
-                    main = driver.findElementByXPath("/html/body/div[4]/div/div/div/div[2]").findElement(By.tagName("section")).findElements(By.xpath("./*")).get(1);
-                    String page = main.findElement(By.className("nap_cat-action-bar")).findElement(By.tagName("form")).findElement(By.tagName("div")).findElement(By.className("pagination")).getAttribute("innerText");
-                    try {
-                        WebElement next = main.findElement(By.className("nap_cat-action-bar")).findElement(By.tagName("form")).findElement(By.tagName("div")).findElement(By.className("pagination")).findElements(By.xpath("./*")).get(4);
-                        executor.executeScript("arguments[0].click();", next.findElement(By.tagName("a")));
-                    } catch (IndexOutOfBoundsException f) {
-                        System.out.println("No paginations");
+        while (true) {
+            try {
+                int count = 1;
+                for (String s : list) {
+                    String cate = getCurrentCategory(count);
+                    System.out.println(cate + "*****************************************");
+                    if (checkStatus(cate)) {
+                        System.out.println("CATEGORY " + cate + " IS ALREADY SCRAPED..");
                         Thread.sleep(10000);
-                        collectLinks(cate);
-                        endCategory = true;
-                        break;
+                        ++count;
+                        continue;
                     }
 
-                    page = page.replace("<", "");
-                    page = page.replace(">", "");
-                    System.out.println(page.split("of")[0].trim() + "-" + page.split("of")[1].trim() + "===========================");
+                    count++;
+                    driver.get(s);
+                    Thread.sleep(5000);
+                    WebElement main = driver.findElementByXPath("/html/body/div[4]/div/div/div/div[2]").findElement(By.tagName("section")).findElements(By.xpath("./*")).get(1);
+                    WebElement select = main.findElement(By.tagName("form")).findElement(By.tagName("div")).findElement(By.className("results")).findElement(By.className("sellers")).findElement(By.tagName("select"));
+                    new Select(select).selectByVisibleText("64");
 
-                    if (page.split("of")[0].trim().equalsIgnoreCase(page.split("of")[1].trim())) {
-                        endCategory = true;
-                        break;
+                    boolean firstAttempt = true;
+                    boolean endCategory = false;
+                    while (true) {
+
+                        while (!firstAttempt) {
+                            Thread.sleep(10000);
+                            main = driver.findElementByXPath("/html/body/div[4]/div/div/div/div[2]").findElement(By.tagName("section")).findElements(By.xpath("./*")).get(1);
+                            String page = main.findElement(By.className("nap_cat-action-bar")).findElement(By.tagName("form")).findElement(By.tagName("div")).findElement(By.className("pagination")).getAttribute("innerText");
+
+                            try {
+                                WebElement next = main.findElement(By.className("nap_cat-action-bar")).findElement(By.tagName("form")).findElement(By.tagName("div")).findElement(By.className("pagination")).findElements(By.xpath("./*")).get(4);
+                                executor.executeScript("arguments[0].click();", next.findElement(By.tagName("a")));
+                            } catch (IndexOutOfBoundsException f) {
+                                System.out.println("No paginations");
+                                Thread.sleep(10000);
+                                collectLinks(cate);
+                                endCategory = true;
+                                break;
+                            }
+
+                            page = page.replace("<", "");
+                            page = page.replace(">", "");
+                            System.out.println(page.split("of")[0].trim() + "-" + page.split("of")[1].trim() + "===========================");
+
+                            if (page.split("of")[0].trim().equalsIgnoreCase(page.split("of")[1].trim())) {
+                                endCategory = true;
+                                break;
+                            }
+
+                            Thread.sleep(10000);
+                            collectLinks(cate);
+                        }
+
+                        if (firstAttempt) {
+                            collectLinks(cate);
+                            firstAttempt = false;
+                        }
+
+
+                        if (endCategory) {
+                            System.out.println("next category");
+                            break;
+                        }
+
                     }
-
-                    Thread.sleep(10000);
-                    collectLinks(cate);
                 }
-
-                if (firstAttempt) {
-                    collectLinks(cate);
-                    firstAttempt = false;
-                }
-
-                if (endCategory) {
-                    System.out.println("next category");
-                    break;
-                }
-
+            } catch (Exception e) {
+                e.printStackTrace();
+                continue;
             }
         }
-
     }
 
-    private String getCurrentCategory(int count) {
+    private boolean checkStatus(String cate) throws Exception {
+        if (cate.equalsIgnoreCase("animal-health")) {
+            return animal_health;
+        } else if (cate.equalsIgnoreCase("prescription-products")) {
+            return prescription_roducts;
+        } else if (cate.equalsIgnoreCase("acupuncture-oriental-medicine")) {
+            return acupuncture_oriental_medicine;
+        } else if (cate.equalsIgnoreCase("personal-care-specialty")) {
+            return personal_care;
+        } else if (cate.equalsIgnoreCase("herbs")) {
+            return herbs;
+        } else if (cate.equalsIgnoreCase("vitamins-minerals")) {
+            return vitamins_and_minerals;
+        } else if (cate.equalsIgnoreCase("nutritional-supplements")) {
+            return nutritional_supplements;
+        } else {
+            return homeopathy;
+        }
+    }
+
+    private String getCurrentCategory(int count) throws Exception {
 
         if (count == 7) {
+            vitamins_and_minerals = true;
             return "nutritional-supplements";
         } else if (count == 6) {
+            herbs = true;
             return "vitamins-minerals";
         } else if (count == 5) {
+            personal_care = true;
             return "herbs";
         } else if (count == 4) {
+            acupuncture_oriental_medicine = true;
             return "personal-care-specialty";
         } else if (count == 3) {
+            prescription_roducts = true;
             return "acupuncture-oriental-medicine";
         } else if (count == 2) {
+            animal_health = true;
             return "prescription-products";
         } else if (count == 1) {
             return "animal-health";
         } else {
+            nutritional_supplements = true;
             return "homeopathy";
         }
     }
 
-    private void collectLinks(String cate) throws InterruptedException {
+    private void collectLinks(String cate) throws Exception {
         Modal modal = null;
 
         WebElement main = driver.findElementByXPath("/html/body/div[4]/div/div/div/div[2]").findElement(By.tagName("section")).findElements(By.xpath("./*")).get(1);
@@ -159,19 +211,18 @@ public class Scrape implements InitializingBean {
         for (String link : links) {
             modal = new Modal();
             driver2.get(link);
-            Thread.sleep(5000);
+            Thread.sleep(10000);
             WebElement imageDiv = driver2.findElement(By.className("content")).findElements(By.xpath("./*")).get(0).findElement(By.tagName("div")).findElements(By.xpath("./*"))
                     // get 1 to next part
                     .get(2).findElements(By.xpath("./*")).get(1).findElements(By.xpath("./*")).get(0).findElements(By.xpath("./*")).get(0).findElement(By.tagName("div"))
                     .findElement(By.className("thumbList")).findElement(By.className("productScroll")).findElement(By.id("galleryThumb")).findElement(By.className("owl-wrapper-outer"))
                     .findElement(By.className("owl-wrapper"));
 
-            for (WebElement href : imageDiv.findElements(By.xpath("./*"))) {
-                String image = href.findElement(By.tagName("div")).findElement(By.tagName("a")).getAttribute("data-image");
-                System.out.println(image);
+            WebElement href = imageDiv.findElements(By.xpath("./*")).get(0);
+            String image = href.findElement(By.tagName("div")).findElement(By.tagName("a")).getAttribute("data-image");
+            System.out.println(image);
 
-                modal.setImages(modal.getImages() + image + ",");
-            }
+            modal.setImages(modal.getImages() + image + ",");
 
 
             WebElement details = driver2.findElement(By.className("content")).findElements(By.xpath("./*")).get(0)
